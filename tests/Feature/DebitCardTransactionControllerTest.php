e4e6a0e36ee8a0861f8c85528456812723b5de4a
Neno Arisma <nenoarisma@gmail.com>
2023-03-23 13:41:10 +0700

    test: customer cannot create a debit card transaction to other customer debit card

--- tests/Feature/DebitCardTransactionControllerTest.php
+++ tests/Feature/DebitCardTransactionControllerTest.php
@@ -104,6 +104,19 @@ class DebitCardTransactionControllerTest extends TestCase
     public function testCustomerCannotCreateADebitCardTransactionToOtherCustomerDebitCard()
     {
         // post /debit-card-transactions
+        $debitCards = DebitCard::factory()->create();
+
+        $this->post('/api/debit-card-transactions', [
+            'debit_card_id' => $debitCards->id,
+            'amount' => 100,
+            'currency_code' => DebitCardTransaction::CURRENCY_IDR,
+        ])->assertForbidden();
+
+        $this->assertDatabaseMissing('debit_card_transactions', [
+            'debit_card_id' => $debitCards->id,
+            'amount' => 100,
+            'currency_code' => DebitCardTransaction::CURRENCY_IDR,
+        ]);
     }
 
     public function testCustomerCanSeeADebitCardTransaction()
