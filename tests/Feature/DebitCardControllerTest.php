e9844802af07a699a1a45855ec6e938b1558ac21
Neno Arisma <nenoarisma@gmail.com>
2023-03-23 13:19:11 +0700

    fix: typo word delelete to delete

--- tests/Feature/DebitCardControllerTest.php
+++ tests/Feature/DebitCardControllerTest.php
@@ -230,7 +230,7 @@ class DebitCardControllerTest extends TestCase
         $this->assertDatabaseHas('debit_cards', [
 
             'id' => $debitCards->id,
-            'deleted_at' => $debitCards->fresh()->delelte_at
+            'deleted_at' => $debitCards->fresh()->delete_at
         ]);
     }
 
